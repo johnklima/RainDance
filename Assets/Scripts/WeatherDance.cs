76baf07f06ace6b707846135b3ccdca59e07591b
johnklima <klima.de.sa@gmail.com>
2025-11-12 11:23:33 +0000

    wind direction visualized

--- Assets/Scripts/WeatherDance.cs
+++ Assets/Scripts/WeatherDance.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Data;
 using System.IO;
 using System;
-using static UnityEngine.Rendering.DebugUI;
+using System.Collections.Generic;
 
 
 public class WeatherDance : MonoBehaviour
@@ -10,6 +10,41 @@ public class WeatherDance : MonoBehaviour
 
     public string filename;
     public DataTable Data;
+    public List<float> windDir = new List<float>();
+
+    float timer = -1;
+    int curRec = 0;
+    Quaternion lastRot;
+    Quaternion nextRot;
+
+    //going to simply say that Z axis in "north" in the case of wind direction
+    //date and time don't matter, as I only care about the intervals, record numbers
+    //then everything can become a float
+    
+     //the columns
+    enum COLS
+    {
+        RecNo,	
+        DateTime,
+        InTemp,
+        InHum,	
+        OutTemp,	
+        OutHum,	
+        OutDew,	
+        OutFeel,	
+        Wind,	
+        Gust,	
+        WindDir,	
+        AbsPressure,	
+        RelPressure,	
+        RainHour,	
+        RainDay,
+        RainWeek,	
+        RainMonth,	
+        RainTotal
+    }
+
+     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,18 +59,53 @@ public class WeatherDance : MonoBehaviour
             int col = 0;
             foreach (var item in dataRow.ItemArray)
             {
+                //so, which columns am I interested in?
                 Debug.Log(col + " " + item);
+                if(col == (int) COLS.WindDir)
+                {
+                    //this is totally fucked up!
+                    float f = float.Parse(item.ToString());
+
+                    windDir.Add(f);
+                }
                 col++;
             }
            
         }
+        //get our start values
+        timer = Time.time;              //now
+        lastRot = transform.rotation;   //looking where
+        nextRot = lastRot;              //and to initialize...
        
     }
 
     // Update is called once per frame
+    float lerptime = 0;
     void Update()
     {
+        if(Time.time - timer >= 1.0f)
+        {
+            timer = Time.time;                          //reset
+            lastRot = transform.rotation;               //looking where now
+            float y = windDir[curRec];                  //get data rec
+            nextRot = Quaternion.Euler(0, y, 0);        //looking where next
+            lerptime = 0;                               //reset lerp
+            //increment and check
+            curRec++;
+            if(curRec >= windDir.Count )
+            {
+                curRec = 0;
+            }
+        }
+
+        //simply interpolate by one second intervals over DT
+        lerptime += Time.deltaTime;
+        if (lastRot != nextRot)
+        {
+            transform.rotation = Quaternion.Lerp(lastRot, nextRot, lerptime);
+        }
         
+
     }
 
     public DataTable ReadFile(string FilePath, char delimiter, bool isFirstRowHeader = true)
