a62bdbcf7fa3542ad979c86a5c4520b554c64a45
John <klima.de.sa@gmail.com>
2026-08-22 10:49:10 +0200

    All Good

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using Alteruna.Multiplayer;
+using Alteruna.Multiplayer.Core;
+
+public class Player : CommunicationBridge
+{
+
+    public float Speed = 5f;
+    public float Sensitivity = 200;
+    
+    private Transform _camera;
+
+    private void Start()
+    {
+        _camera = GetComponentInChildren<Camera>().transform;
+    }
+
+    public override void Possessed(bool isMe, User user)
+    {
+        // disables this script for remote players
+        enabled = isMe;
+    }
+
+    public void Update()
+    {
+        UpdateMovement();
+        UpdateRotation();
+    }
+
+    private void UpdateMovement()
+    {
+        // Get horizontal input
+        var input = Input.GetAxisRaw("Horizontal") * transform.right;
+
+        // Get vertical input
+        input += Input.GetAxisRaw("Vertical") * transform.forward;
+        
+        if (input.magnitude < 0.01f) return;
+        
+        // Normalize input
+        input /= input.magnitude;
+        
+        // Scale input
+        input *= Speed * Time.deltaTime;
+        
+        // Apply input
+        transform.Translate(input, Space.World);
+    }
+
+    private void UpdateRotation()
+    {
+        float sensitivity = Sensitivity * Time.deltaTime;
+		
+        // Vertical mouse look
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX * sensitivity, 0);
+		
+        // vertical mouse look
+        
+        // Get current camera rotation
+        Vector3 cameraRot = _camera.localEulerAngles;
+        // Add mouse movement
+        cameraRot.x -= Input.GetAxis("Mouse Y") * sensitivity;
+        // Fix overrotation issues
+        if (cameraRot.x > 180f) cameraRot.x -= 360f;
+        // Clamp input so you can't look behind you
+        cameraRot.x = Mathf.Clamp(cameraRot.x, -89f, 89f);
+        // apply rotation
+        _camera.localEulerAngles = cameraRot;
+    }
+}
--- Assets/Scripts/WeatherDance.cs
+++ Assets/Scripts/WeatherDance.cs
@@ -73,7 +73,7 @@ public class WeatherDance : MonoBehaviour
 
         anim = GetComponent<Animator>();
 
-
+        
         filename = Application.dataPath + "/Data/weather.txt";
         Data = ReadFile(filename,'|', true);
 
@@ -311,5 +311,58 @@ public class WeatherDance : MonoBehaviour
         }
     }
 
+    public DataTable WriteFile (string FilePath, char delimiter, bool isFirstRowHeader = true)
+    {
+        //TODO: do this
 
+        try
+        {
+            //Create object of Datatable
+            DataTable objDt = new DataTable();
+
+            //File.WriteAllText;
+
+            //Open and Read Delimited Text File
+            using (TextReader tr = File.OpenText(FilePath))
+            {
+                string line;
+                //Read all lines from file
+                while ((line = tr.ReadLine()) != null)
+                {
+                    //Debug.Log(line);
+
+                    //split data based on delimiter/separator and convert it into string array
+                    string[] arrItems = line.Split(delimiter);
+
+                    //Check for first row of file
+                    if (objDt.Columns.Count == 0)
+                    {
+                        // If first row of text file is header then, we will not put that data into datarow and consider as column name 
+                        if (isFirstRowHeader)
+                        {
+                            for (int i = 0; i < arrItems.Length; i++)
+                                objDt.Columns.Add(new DataColumn(Convert.ToString(arrItems[i]), typeof(string)));
+                            continue;
+                        }
+                        else
+                        {
+                            // Create the data columns for the data table based on the number of items on the first line of the file
+                            for (int i = 0; i < arrItems.Length; i++)
+                                objDt.Columns.Add(new DataColumn("Column" + Convert.ToString(i), typeof(string)));
+
+                        }
+                    }
+
+                    //Insert data from text file to datarow
+                    objDt.Rows.Add(arrItems);
+                }
+            }
+            //return datatable
+            return objDt;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
 }
