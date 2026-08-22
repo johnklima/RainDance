d95da85a2f9d0e697b8286fe7fc203593cfc5895
John <klima.de.sa@gmail.com>
2026-08-22 17:04:57 +0200

    borky borky borky

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -1,4 +1,5 @@
 using Alteruna.Multiplayer;
+using Alteruna.Multiplayer.Core;
 using System.Security.Claims;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,24 +9,42 @@ public class CharacterSelect : CommunicationBridge
 {
     public Transform[] Characters;
     public Text debug;
+    Alteruna.Multiplayer.Avatar avatar;
+    Alteruna.Multiplayer.Spawner spawner;
+
+    private void Awake()
+    {
+        avatar = GetComponent<Alteruna.Multiplayer.Avatar>();
+        spawner = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Spawner>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (!avatar.IsMe)
+            return;
 
         int c = Multiplayer.GetUsers().Count;
         debug.text = ("players " + c);
 
-
-        Transform av = GameObject.Instantiate(Characters[c-1]);
-            
-        av.parent = transform;
-        av.position = Vector3.zero;
+        SpawnCharacter();       
 
     }
-
-    // Update is called once per frame
-    void Update()
+    void SpawnCharacter()
     {
-        
+        int c = Multiplayer.GetUsers().Count;
+        GameObject avy = spawner.Spawn(c - 1);
+
+        avy.transform.parent = avatar.transform;
+
+    }
+    void InstanceCharacter() 
+    {
+        int c = Multiplayer.GetUsers().Count;
+
+        Transform av = GameObject.Instantiate(Characters[c - 1]);
+
+        av.parent = transform;
+        av.position = Vector3.zero;
     }
 }
